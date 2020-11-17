74d569b321759b06c654f44bf4b0cee6338c2a9f
Mosharraf Hossain <mail2mhossain@gmail.com>
2020-11-17 11:22:08 +0600

    Otto Group Product Classofication

---
+++ tpot_Otto_best_model.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import train_test_split
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=1)
+
+# Average CV score on the training set was: 0.7004355140018216
+exported_pipeline = SGDClassifier(alpha=0.01, eta0=0.01, fit_intercept=True, l1_ratio=1.0, learning_rate="constant", loss="modified_huber", penalty="elasticnet", power_t=50.0)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 1)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
