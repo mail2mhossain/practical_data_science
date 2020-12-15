1601ae547c62330ddde635b814ce2bcd7b7a11be
Mosharraf Hossain <mail2mhossain@gmail.com>
2020-12-15 10:44:33 +0600

    pandas_profiling

--- tpot_Otto_best_model.py
+++ tpot_Otto_best_model.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import StandardScaler
+from tpot.builtins import StackingEstimator, ZeroCount
+from xgboost import XGBClassifier
+from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -9,11 +15,16 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=1)
 
-# Average CV score on the training set was: 0.7004355140018216
-exported_pipeline = SGDClassifier(alpha=0.01, eta0=0.01, fit_intercept=True, l1_ratio=1.0, learning_rate="constant", loss="modified_huber", penalty="elasticnet", power_t=50.0)
-# Fix random state in exported estimator
-if hasattr(exported_pipeline, 'random_state'):
-    setattr(exported_pipeline, 'random_state', 1)
+# Average CV score on the training set was: 0.9840384615384615
+exported_pipeline = make_pipeline(
+    ZeroCount(),
+    StandardScaler(),
+    StackingEstimator(estimator=XGBClassifier(learning_rate=1.0, max_depth=10, min_child_weight=16, n_estimators=100, nthread=1, subsample=0.2)),
+    StackingEstimator(estimator=SGDClassifier(alpha=0.01, eta0=0.1, fit_intercept=False, l1_ratio=0.5, learning_rate="invscaling", loss="log", penalty="elasticnet", power_t=0.0)),
+    MLPClassifier(alpha=0.001, learning_rate_init=0.001)
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 1)
 
 exported_pipeline.fit(training_features, training_target)
 results = exported_pipeline.predict(testing_features)
